bb618e42911adb212eb652ed869f1bc5235bf5ab
bambiengineer <banbi.rice.cracker@gmail.com>
2021-01-18 23:04:33 +0900

    add/ABC_157_A_B_C

---
+++ 157_B_Bingo.py
@@ -0,0 +1,23 @@
+# https://atcoder.jp/contests/abc157/tasks/abc157_b
+
+import numpy as np
+A = [list(map(int, input().split())) for _ in range(3)]
+N = int(input())
+b = [int(input()) for _ in range(N)]
+cnt = np.zeros((3,3))
+for i in b:
+    for j, a in enumerate(A):
+        for k, aa in enumerate(a):
+            if i == aa:
+                cnt[j][k] = 1
+            else:
+                continue
+
+column_sum = np.sum(cnt, axis=0)
+row_sum = np.sum(cnt, axis=1)
+if 3 in column_sum or 3 in row_sum:
+    print("Yes")
+elif cnt[0][0] == cnt[1][1] == cnt[2][2] == 1 or cnt[0][2] == cnt[1][1] == cnt[2][0] == 1:
+    print("Yes")
+else:
+    print("No")
---
+++ 157_C_GuessTheNumber.py
@@ -0,0 +1,22 @@
+# https://atcoder.jp/contests/abc157/tasks/abc157_c
+
+N, M = map(int, input().split())
+sc = [list(map(int, input().split())) for _ in range(M)]
+ans_l = [False]*N
+for s, c in sc:
+    if ans_l[s-1] and ans_l[s-1] != c:
+        print(-1)
+        exit()
+    elif N > 1 and s == 1 and c == 0:
+        print(-1)
+        exit()
+    else:
+        ans_l[s-1] = c
+ans = 0
+for j, a in enumerate(ans_l):
+    if not a:
+        if N > 1 and j == 0:
+            ans += 1 * (10**(N-1-j))
+    else:
+        ans += a * (10**(N-1-j))   
+print(ans)
