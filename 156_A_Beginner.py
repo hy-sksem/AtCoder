c34251f0278b314bd17b93232cd0890b23c38a4b
bambiengineer <banbi.rice.cracker@gmail.com>
2021-01-19 20:42:21 +0900

    add/ABC_156_A_B_C

---
+++ 156_A_Beginner.py
@@ -0,0 +1,4 @@
+# https://atcoder.jp/contests/abc156/tasks/abc156_a
+
+N, R = map(int, input().split())
+print(R) if N >= 10 else print(R+(100*(10-N)))
---
+++ 156_B_Digits.py
@@ -0,0 +1,6 @@
+# https://atcoder.jp/contests/abc156/tasks/abc156_b
+
+import math
+N, K = map(int, input().split())
+x = math.log(N, K) if N != 1 else 1
+print(math.ceil(x))
