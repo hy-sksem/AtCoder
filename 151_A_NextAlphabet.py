d4f8a6fbe695521067ce5ce3f003878561d5443a
bambiengineer <banbi.rice.cracker@gmail.com>
2021-01-27 19:49:16 +0900

    add/ABC_151_A_B_C

---
+++ 151_A_NextAlphabet.py
@@ -0,0 +1,6 @@
+# https://atcoder.jp/contests/abc151/tasks/abc151_a
+
+import string
+C = input()
+l = string.ascii_lowercase
+print(l[l.index(C)+1])
---
+++ 151_B_AchieveTheGoal.py
@@ -0,0 +1,10 @@
+# https://atcoder.jp/contests/abc151/tasks/abc151_b
+
+N, K, M = map(int, input().split())
+A = list(map(int, input().split()))
+if 0 <= M*N - sum(A) <= K:
+    print(M*N - sum(A))
+elif M*N - sum(A) < 0:
+    print(0)
+else:
+    print(-1)
