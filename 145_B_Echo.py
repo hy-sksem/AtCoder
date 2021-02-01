e5f24a4497daa08757248b91473a22fb3c1ef26a
bambiengineer <banbi.rice.cracker@gmail.com>
2021-02-01 18:14:05 +0900

    add/ABC_145_A_B_C

---
+++ 145_B_Echo.py
@@ -0,0 +1,8 @@
+# https://atcoder.jp/contests/abc145/tasks/abc145_b
+
+N = int(input())
+S = input()
+
+first_h = S[:N//2]
+second_h = S[N//2:]
+print("Yes" if first_h == second_h else "No")
---
+++ 145_C_AverageLength.py
@@ -0,0 +1,22 @@
+# https://atcoder.jp/contests/abc145/tasks/abc145_c
+
+import math
+import itertools
+
+def distance(X, Y):
+    return math.sqrt((X[0]-Y[0])**2 + (X[1]-Y[1])**2)
+
+N = int(input())
+k = [i for i in range(N)]
+xy = [tuple(map(int, input().split())) for _ in range(N)]
+l = list(itertools.permutations(k))
+
+ans = 0
+for j in range(len(l)):
+    cnt = 0
+    for k in range(N-1):
+        cnt += distance(xy[l[j][k]], xy[l[j][k+1]])
+    ans += cnt
+print(ans/len(l))
+        
+
