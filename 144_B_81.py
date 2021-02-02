4fcabf2cbe16ef3013dd0a61ddc8c28df3c6dceb
bambiengineer <banbi.rice.cracker@gmail.com>
2021-02-02 18:09:17 +0900

    add/ABC_144_A_B_C

---
+++ 144_B_81.py
@@ -0,0 +1,9 @@
+# https://atcoder.jp/contests/abc144/tasks/abc144_b
+
+N = int(input())
+for i in range(1, 10):
+    for j in range(1, 10):
+        if i*j == N:
+            print("Yes")
+            exit()
+print("No")
---
+++ 144_C_WalkOnMultiplicationTable.py
@@ -0,0 +1,11 @@
+# https://atcoder.jp/contests/abc144/tasks/abc144_c
+
+import math
+N = int(input())
+n_r = math.floor(math.sqrt(N))
+while True:
+    if N % n_r == 0:
+        print(int(n_r+(N/n_r)-2))
+        exit()
+    else:
+        n_r -= 1
