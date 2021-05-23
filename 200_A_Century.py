9d84e3eadc61909f76f10f0b35eae904aefa16a4
Shikasembei <banbi.rice.cracker@gmail.com>
2021-05-23 18:07:49 +0900

    add/ABC_200_A_B_C

---
+++ 200_A_Century.py
@@ -0,0 +1,2 @@
+n = int(input())
+print(n//100+1 if n%100 !=0 else n//100)
---
+++ 200_C_Ringo'sFaveriteNumbers.py
@@ -0,0 +1,9 @@
+n = int(input())
+a = list(map(int, input().split()))
+b = [0]*200
+for i in a:
+    b[i%200] += 1
+ans = 0
+for j in b:
+    ans += (j*(j-1))/2
+print(int(ans))
