61e8d603e52201129313a89dd6a7c1c3ddf299fb
hy-sksem <banbi.rice.cracker@gmail.com>
2021-09-27 23:10:52 +0900

    add/ABC_220 A B C

---
+++ 220_A_FindMultiple.py
@@ -0,0 +1,10 @@
+a, b, c = map(int, input().split())
+
+for i in range(1, 1000):
+    n = c * i
+    if a <= n and n <= b:
+        print(n)
+        exit()
+    else: 
+        continue
+print(-1)
---
+++ 220_C_LongSequence.py
@@ -0,0 +1,20 @@
+n = int(input())
+a = list(map(int, input().split()))
+x = int(input())
+
+sum = 0
+sum_l = []
+
+for i in range(n):
+    sum += a[i]
+    sum_l.append(sum)
+    if sum > x:
+        print(i+1)
+        exit()
+
+q, x = divmod(x, sum)
+for j in range(len(sum_l)):
+    temp = x - sum_l[j]
+    if temp < 0:
+        print(n*q + j+1)
+        exit()
