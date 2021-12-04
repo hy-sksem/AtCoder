13d03ab73c37f6f976baf146f63bc19cd69b21e6
hy-sksem <banbi.rice.cracker@gmail.com>
2021-12-04 10:30:48 +0900

    add/ABC_230 A B C

---
+++ 230_B_TripleMetre.py
@@ -0,0 +1,3 @@
+S = input()
+T = "oxx" * 100000
+print("Yes" if S in T else "No")
---
+++ 230_C_XDrawing.py
@@ -0,0 +1,15 @@
+N, A, B = map(int, input().split())
+P, Q, R, S = map(int, input().split())
+
+F = [["."] * (S-R+1) for _ in range(Q-P+1)]
+
+for i in range(Q-P+1):
+    for j in range(S-R+1):
+        x = i+P
+        y = j+R
+
+        if abs(x-A) == abs(y-B):
+            F[i][j] = "#"
+
+for f in F:
+    print("".join(f))
