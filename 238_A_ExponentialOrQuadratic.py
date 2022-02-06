cb5f15c8ff2cfb959665765a206e4fe1efa1ca50
hy-sksem <banbi.rice.cracker@gmail.com>
2022-02-06 11:09:33 +0900

    add/ABC_238 A B C

---
+++ 238_A_ExponentialOrQuadratic.py
@@ -0,0 +1,2 @@
+N = int(input())
+print("Yes" if N >= 5 or N == 1 else "No")
---
+++ 238_B_Pizza.py
@@ -0,0 +1,17 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+D = [False] * 360
+D[0] = True
+
+index = 0
+for a in A:
+    index = index + a if index + a <= 359 else index + a - 360
+    D[index] = True
+
+D = [i for i, x in enumerate(D) if x]
+D.append(360)
+ans = 0
+for i in range(len(D)-1):
+    ans = D[i+1] - D[i] if D[i+1] - D[i] >= ans else ans
+print(ans) 
