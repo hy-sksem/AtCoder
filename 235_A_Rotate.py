ec4861924c05ba54cf74f6e2e21fd05cef9fde6a
hy-sksem <banbi.rice.cracker@gmail.com>
2022-02-07 21:53:41 +0900

    add/ABC_235 A B C

---
+++ 235_A_Rotate.py
@@ -0,0 +1,5 @@
+abc = input()
+a = abc[0]
+b = abc[1]
+c = abc[2]
+print(int(a)*111+int(b)*111+int(c)*111)
---
+++ 235_B_ClimbingTakahashi.py
@@ -0,0 +1,10 @@
+N = int(input())
+H = list(map(int, input().split()))
+ans = 0
+for i in range(N-1):
+    if H[i+1] > H[i]:
+        continue
+    else:
+        print(H[i])
+        exit()
+print(H[-1])
