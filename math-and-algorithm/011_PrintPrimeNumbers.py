f4c9b2ad8cbf868f84187e2008321df46cc5159b
hy-sksem <banbi.rice.cracker@gmail.com>
2022-02-12 14:23:30 +0900

    add/010 011

---
+++ math-and-algorithm/011_PrintPrimeNumbers.py
@@ -0,0 +1,9 @@
+def is_prime(n):
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+ 
+N = int(input())
+ans = [i for i in range(2, N+1) if is_prime(i)]
+print(*ans, " ")
