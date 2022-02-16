47b823058779870d82807b2d33a6e1cda91e6ce6
hy-sksem <banbi.rice.cracker@gmail.com>
2022-02-16 22:49:13 +0900

    add/021 022 023

---
+++ math-and-algorithm/022_chooseCards3.py
@@ -0,0 +1,12 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+cnt = [0 for i in range(100000)]
+for a in A:
+    cnt[a] += 1
+
+ans = 0
+for i in range(1, 50000):
+    ans += cnt[i] * cnt[100000 - i]
+ans += cnt[50000] * (cnt[50000] - 1) // 2
+print(ans)
