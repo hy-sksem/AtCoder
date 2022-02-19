fb3efe419413517079a5754f9adf356aa58c6176
hy-sksem <banbi.rice.cracker@gmail.com>
2022-02-19 15:22:49 +0900

    add/027 028 029

---
+++ math-and-algorithm/028_Frog1.py
@@ -0,0 +1,11 @@
+N = int(input())
+H = list(map(int, input().split()))
+
+dp = [0 for i in range(N)]
+dp[0] = 0
+for i in range(1, N):
+    if i > 1:
+        dp[i] = min(dp[i-1]+abs(H[i] - H[i-1]), dp[i-2]+abs(H[i] - H[i-2]))
+    else:
+        dp[i] = dp[i-1]+abs(H[i] - H[i-1])
+print(dp[-1])
---
+++ math-and-algorithm/029_ClimbStairs.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+dp = [0 for _ in range(N+1)]
+dp[0] = 1
+for i in range(1, N+1):
+    if i >= 2:
+        dp[i] = dp[i-1] + dp[i-2]
+    else:
+        dp[i] = 1
+print(dp[-1])
