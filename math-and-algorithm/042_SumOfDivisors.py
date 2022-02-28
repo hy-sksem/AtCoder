ca1a2cd04c6e667995ac26ff41509c02f9767cd4
hy-sksem <banbi.rice.cracker@gmail.com>
2022-02-28 21:49:12 +0900

    add/041 042 043

---
+++ math-and-algorithm/042_SumOfDivisors.py
@@ -0,0 +1,11 @@
+N = int(input())
+F = [0 for i in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, (N//i) + 1):
+        F[i*j] += 1
+
+ans = 0
+for i in range(1, N+1):
+    ans += i * F[i]
+print(ans)
---
+++ math-and-algorithm/043_IsItConnected.py
@@ -0,0 +1,29 @@
+import sys
+
+def DFS(pos):
+    visited[pos] = True
+    for i in G[pos]:
+        if not visited[i]:
+            DFS(i)
+
+sys.setrecursionlimit(120000)
+
+N, M = map(int, input().split())
+A = [ None ] * M
+B = [ None ] * M
+for i in range(M):
+    A[i], B[i] = map(int, input().split())
+
+G = [ list() for i in range(N+1) ]
+for i in range(M):
+    G[A[i]].append(B[i])
+    G[B[i]].append(A[i])
+visited = [ False ] * (N+1)
+DFS(1)
+
+ans = True
+for i in range(1, N+1):
+    if not visited[i]:
+        ans = False
+        break
+print('The graph is connected.' if ans else 'The graph is not connected.')
