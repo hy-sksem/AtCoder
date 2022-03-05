8a1a991631c096a5f5f33f55c981b60312eaaf78
hy-sksem <banbi.rice.cracker@gmail.com>
2022-03-05 12:45:46 +0900

    add/056 057 058

---
+++ math-and-algorithm/056_RucurrenceFormula2.py
@@ -0,0 +1,33 @@
+from copy import deepcopy
+
+MOD = 10**9 + 7
+
+def multiply(A, B):
+    C = [[0,0,0],[0,0,0],[0,0,0]]
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                C[i][j] += A[i][k] * B[k][j]
+                C[i][j] %= MOD
+    return C
+
+def power(A, n):
+    P = deepcopy(A)
+    Q = [[0,0,0],[0,0,0],[0,0,0]]
+    flag = False
+    for i in range(60):
+        if n & (1 << i):
+            if not flag:
+                Q = deepcopy(P)
+                flag = True
+            else:
+                Q = deepcopy(multiply(Q, P))
+        P = deepcopy(multiply(P, P))
+    return Q
+
+N = int(input())
+A = [[1,1,1], [1,0,0], [0,1,0]]
+B = power(A, N-1)
+
+ans = (2*B[2][0] + B[2][1] + B[2][2]) % MOD
+print(ans)
---
+++ math-and-algorithm/058_MoveOnSqueres.py
@@ -0,0 +1,3 @@
+N, X, Y = map(int, input().split())
+
+print("Yes" if (X+Y)%2==N%2 and abs(X+Y)<=N else "No")
