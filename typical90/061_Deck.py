593a6477c082ff3cf814aeb2364a2da11296f99c
hs <banbi.rice.cracker@gmail.com>
2022-04-22 22:40:36 +0900

    add/typical90 061

---
+++ typical90/061_Deck.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+dq = deque()
+Q = int(input())
+for i in range(Q):
+    t, x = map(int, input().split())
+    if t == 1:
+        dq.appendleft(x)
+    if t == 2:
+        dq.append(x)
+    if t == 3:
+        print(dq[x-1])
