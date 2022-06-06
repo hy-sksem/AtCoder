9a15e7b223df38771a422c6d86df4ce17693ca15
hs <banbi.rice.cracker@gmail.com>
2022-06-06 22:14:14 +0900

    update/ABC_254 D

--- ABC/abc254/d/main.py
+++ ABC/abc254/d/main.py
@@ -10,7 +10,6 @@ for i in range(2, N + 1):
         while d[j] % i == 0:
             d[j] //= i  # 平方数で割った値を残す
 
-print(d, square_num)
 ans = 0
 for i in range(1, N + 1):
     # i*jが平方数となるj、つまりd[i]*jが平方数となる1以上N以下の整数jの個数はjが平方数となる1以上N/d[i]以下の整数jの個数と一致
