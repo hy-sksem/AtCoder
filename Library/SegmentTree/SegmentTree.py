ff5e819ec047907ac1d8050617f6aae01eec80b4
hy-sksem <banbi.rice.cracker@gmail.com>
2022-07-03 19:29:39 +0900

    update/SegmentTree

--- Library/SegmentTree/SegmentTree.py
+++ Library/SegmentTree/SegmentTree.py
@@ -1,32 +1,13 @@
 from typing import List, Union, Any, Callable
 
 
-def ceil_pow2(n: int) -> int:
-    x = 0
-    while (1 << x) < n:
-        x += 1
-
-    return x
-
-
 class SegTree:
-    """
-    SegmentTreeクラス
-    """
-
     def __init__(
         self,
         op: Callable[[Any, Any], Any],
         e: Any,
         v: Union[int, List[Any]],
     ) -> None:
-        """
-        SegmentTreeクラスのプロパティ
-        args:
-            op: Callable[[Any, Any], Any]
-            e: Any
-            v: Union[int, List[Any]]
-        """
         self._op = op
         self._e = e
 
@@ -34,86 +15,49 @@ class SegTree:
             v = [e] * v
 
         self._n = len(v)
-        self._log = ceil_pow2(self._n)
+        self._log = (self._n - 1).bit_length()
         self._size = 1 << self._log
-        self._d = [e] * (2 * self._size)
-
+        self._df = [e] * (2 * self._size)
         for i in range(self._n):
-            self._d[self._size + i] = v[i]
+            self._df[self._size + i] = v[i]
         for i in range(self._size - 1, 0, -1):
             self._update(i)
 
     def set(self, p: int, x: Any) -> None:
-        """
-        p番目の要素をxに変更する
-        args:
-            p: int
-            x: Any
-        """
         assert 0 <= p < self._n
 
         p += self._size
-        self._d[p] = x
+        self._df[p] = x
+        # 親のノードを更新
         for i in range(1, self._log + 1):
             self._update(p >> i)
 
     def get(self, p: int) -> Any:
-        """
-        p番目の要素を返す
-        args:
-            p: int
-        returns:
-            Any
-        """
         assert 0 <= p < self._n
-
-        return self._d[p + self._size]
+        return self._df[p + self._size]
 
     def prod(self, left: int, right: int) -> Any:
-        """
-        leftからrightまでの積を返す
-        args:
-            left: int
-            right: int
-        returns:
-            Any
-        """
-
         assert 0 <= left <= right <= self._n
-        sml = self._e
-        smr = self._e
+        sml, smr = self._e, self._e
         left += self._size
         right += self._size
 
         while left < right:
             if left & 1:
-                sml = self._op(sml, self._d[left])
+                sml = self._op(sml, self._df[left])
                 left += 1
             if right & 1:
                 right -= 1
-                smr = self._op(self._d[right], smr)
+                smr = self._op(self._df[right], smr)
             left >>= 1
             right >>= 1
 
         return self._op(sml, smr)
 
     def all_prod(self) -> Any:
-        """
-        全ての積を返す
-        returns:
-            Any
-        """
-        return self._d[1]
+        return self._df[1]
 
     def max_right(self, left: int, f: Callable[[Any], bool]) -> int:
-        """
-        leftから右端までの最大の要素を返す
-        args:
-            left: int
-            f: Callable[[Any], bool]
-        returns:
-            int
-        """
         assert 0 <= left <= self._n
         assert f(self._e)
 
@@ -128,27 +72,19 @@ class SegTree:
             first = False
             while left % 2 == 0:
                 left >>= 1
-            if not f(self._op(sm, self._d[left])):
+            if not f(self._op(sm, self._df[left])):
                 while left < self._size:
                     left *= 2
-                    if f(self._op(sm, self._d[left])):
-                        sm = self._op(sm, self._d[left])
+                    if f(self._op(sm, self._df[left])):
+                        sm = self._op(sm, self._df[left])
                         left += 1
                 return left - self._size
-            sm = self._op(sm, self._d[left])
+            sm = self._op(sm, self._df[left])
             left += 1
 
         return self._n
 
     def min_left(self, right: int, f: Callable[[Any], bool]) -> int:
-        """
-        rightから左端までの最小の要素を返す
-        args:
-            right: int
-            f: Callable[[Any], bool]
-        returns:
-            int
-        """
         assert 0 <= right <= self._n
         assert f(self._e)
 
@@ -164,21 +100,16 @@ class SegTree:
             right -= 1
             while right > 1 and right % 2:
                 right >>= 1
-            if not f(self._op(self._d[right], sm)):
+            if not f(self._op(self._df[right], sm)):
                 while right < self._size:
                     right = 2 * right + 1
-                    if f(self._op(self._d[right], sm)):
-                        sm = self._op(self._d[right], sm)
+                    if f(self._op(self._df[right], sm)):
+                        sm = self._op(self._df[right], sm)
                         right -= 1
                 return right + 1 - self._size
-            sm = self._op(self._d[right], sm)
+            sm = self._op(self._df[right], sm)
 
         return 0
 
     def _update(self, k: int) -> None:
-        """
-        k番目の要素を更新する
-        args:
-            k: int
-        """
-        self._d[k] = self._op(self._d[2 * k], self._d[2 * k + 1])
+        self._df[k] = self._op(self._df[2 * k], self._df[2 * k + 1])
