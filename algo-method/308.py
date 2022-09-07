1f49bfe4babc21526b55c60f81bb9d068d743dbb
hy-sksem <45358277+hy-sksem@users.noreply.github.com>
2022-09-07 21:46:28 +0900

    add/algo 308

---
+++ algo-method/308.py
@@ -0,0 +1,12 @@
+N, W = map(int, input().split())
+dp = [[0] * (W + 1) for _ in range(N + 1)]
+
+for i in range(N):
+    w, v = map(int, input().split())
+    for j in range(W + 1):
+        if j >= w:
+            dp[i + 1][j] = max(dp[i][j - w] + v, dp[i][j])
+        else:
+            dp[i + 1][j] = dp[i][j]
+
+print(dp[N][W])
