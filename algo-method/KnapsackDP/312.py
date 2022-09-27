32793b50fe0097f3d81fef10348094144a56b9e1
hy-sksem <45358277+hy-sksem@users.noreply.github.com>
2022-09-27 21:50:40 +0900

    add/algo 312

---
+++ algo-method/KnapsackDP/312.py
@@ -0,0 +1,24 @@
+N, M, K = map(int, input().split())
+A = list(map(int, input().split()))
+
+dp = [10**18] * (M + 1)
+dp[0] = 0
+
+for i in range(N):
+    a = A[i]
+    for j in range(M, -1, -1):
+        if j - a >= 0:
+            dp[j] = min(dp[j], dp[j - a] + 1)
+print("Yes" if dp[M] <= K else "No")
+
+## 2重ループ
+# dp = [[10**18] * (M + 1) for _ in range(N + 1)]
+# dp[0][0] = 0
+
+# for i in range(N):
+#     a = A[i]
+#     for j in range(M + 1):
+#         if j - a >= 0:
+#             dp[i + 1][j] = min(dp[i + 1][j], dp[i][j - a] + 1)
+#         dp[i + 1][j] = min(dp[i + 1][j], dp[i][j])
+# print("Yes" if dp[N][M] <= K else "No")
