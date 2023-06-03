973e42a72d90fe04b1f39b1adba528228036c1e1
hy-sksem <45358277+hy-sksem@users.noreply.github.com>
2023-06-03 23:29:32 +0900

    add/ABC_304 e

---
+++ ABC/abc304/e/main.py
@@ -0,0 +1,143 @@
+class UnionFind:
+    """Union Find木クラス
+    Attributes:
+        parent (list): 親要素を格納するリスト。親要素は自身のサイズを格納する。
+    Notes:
+        - parent[i] < 0 の場合、iは根。この時、-parent[i]はそのグループのサイズ
+        - parent[i] = -1 とすると、iは独立。
+    """
+
+    def __init__(self, n):
+        """
+        Args:
+            n (int): The number of objects. 要素数
+        """
+        self.n = n
+        self.parents = [-1] * n
+
+    def find(self, x):
+        """returns the root of x
+
+        Args:
+            x (int): The object to find the root of. 要素
+
+        Returns:
+            int: The root of x.
+        """
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y):
+        """unites the root of x and the root of y
+
+        Args:
+            x (int): The first object to union. 要素
+            y (int): The second object to union. 要素
+        """
+        x = self.find(x)
+        y = self.find(y)
+        if x == y:  # すでに同じ親
+            return
+        # union by size
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def size(self, x):
+        """returns the size of the group of x
+
+        Args:
+            x (int): The object to find the size of. 要素
+
+        Returns:
+            int: The size of the group of x.
+        """
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        """returns True if the root of x is the same as the root of y
+
+        Args:
+            x (int): The first object to check. 要素
+            y (int): The second object to check. 要素
+
+        Returns:
+            bool: True if the root of x is the same as the root of y.
+        """
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        """returns the members of the group of x
+
+        Args:
+            x (int): The object to find the members of. 要素
+
+        Returns:
+            list: The members of the group of x.
+        """
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        """returns the roots
+
+        Returns:
+            list: The roots.
+        """
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        """returns the number of groups
+
+        Returns:
+            int: The number of groups.
+        """
+        return len(self.roots())
+
+    def all_group_members(self):
+        """returns all groups
+
+        Returns:
+            list: All group members.
+        """
+        return {r: self.members(r) for r in self.roots()}
+
+
+def int1(x: int) -> int:
+    return int(x) - 1
+
+
+N, M = map(int, input().split())
+uv = [list(map(int1, input().split())) for _ in range(M)]
+K = int(input())
+xy = set()
+for i in range(K):
+    x, y = map(int1, input().split())
+    xy.add(tuple([x, y]))
+
+Q = int(input())
+pq = [list(map(int1, input().split())) for _ in range(Q)]
+
+uf = UnionFind(N)
+for u, v in uv:
+    uf.union(u, v)
+
+out = set()
+for x, y in xy:
+    n = uf.find(x)
+    m = uf.find(y)
+    out.add(tuple((n, m)))
+    out.add(tuple((m, n)))
+
+for p, q in pq:
+    if uf.same(p, q):
+        print("Yes")
+    else:
+        ans = "Yes"
+        if tuple((uf.find(p), uf.find(q))) in out:
+            ans = "No"
+        print(ans)
